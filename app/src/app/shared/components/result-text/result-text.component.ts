ec2896ceecd01ec5b776cb9633a43db51eb6003f
Gion-Andri Cantieni <gion-andri@gmx.ch>
2021-10-21 11:14:08 +0200

    feat: redirect words starting with cf.

---
+++ app/src/app/shared/components/result-text/result-text.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-result-text',
+  templateUrl: './result-text.component.html',
+  styleUrls: ['./result-text.component.scss'],
+})
+export class ResultTextComponent implements OnInit {
+  @Input()
+  public text: string;
+
+  @Input()
+  public searchString: string;
+
+  @Output()
+  public changeSearchTerm = new EventEmitter<string>();
+
+  public displayText: string;
+  public isRedirect = false;
+
+  constructor() {}
+
+  ngOnInit() {
+    if (this.text.startsWith('cf. ')) {
+      this.isRedirect = true;
+      this.displayText = this.text.replace('cf. ', '');
+    } else {
+      this.displayText = this.text;
+    }
+  }
+}
