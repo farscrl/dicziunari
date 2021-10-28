c0e98bd3092cb789e3fa90a3390efd100cb88a3a
Gion-Andri Cantieni <gion-andri@gmx.ch>
2021-10-28 16:52:48 +0200

    feat: limit screen orientation for phones to portrait mode

--- app/android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ app/android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,5 +1,17 @@
 package io.ionic.starter;
 
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    if(getResources().getBoolean(R.bool.portrait_only)){
+      setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+  }
+}
