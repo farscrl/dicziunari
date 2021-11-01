b4380ec3bc5cb0233be477d3929f6ad9f8ba6850
Gion-Andri Cantieni <gion-andri@gmx.ch>
2021-11-01 15:02:52 +0100

    chore: remove unused module

--- app/src/app/favourites/favourites.module.ts
+++ app/src/app/favourites/favourites.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { FavouritesPage } from './favourites.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 import { TranslateModule } from '@ngx-translate/core';
 import { FavouriteItemComponent } from './favourite-item/favourite-item.component';
 import { SharedModule } from '../shared/shared.module';
@@ -11,15 +10,7 @@ import { SharedModule } from '../shared/shared.module';
 import { FavouritesPageRoutingModule } from './favourites-routing.module';
 
 @NgModule({
-  imports: [
-    IonicModule,
-    CommonModule,
-    FormsModule,
-    ExploreContainerComponentModule,
-    FavouritesPageRoutingModule,
-    TranslateModule,
-    SharedModule,
-  ],
+  imports: [IonicModule, CommonModule, FormsModule, FavouritesPageRoutingModule, TranslateModule, SharedModule],
   declarations: [FavouritesPage, FavouriteItemComponent],
 })
 export class FavouritesPageModule {}
--- app/src/app/search/search.module.ts
+++ app/src/app/search/search.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SearchPage } from './search.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 import { TranslateModule } from '@ngx-translate/core';
 import { SearchResultComponent } from './search-result/search-result.component';
 import { SharedModule } from './../shared/shared.module';
@@ -13,15 +12,7 @@ import { SearchPageRoutingModule } from './search-routing.module';
 import { SearchDetailPage } from './search-detail/search-detail.page';
 
 @NgModule({
-  imports: [
-    IonicModule,
-    CommonModule,
-    FormsModule,
-    ExploreContainerComponentModule,
-    SearchPageRoutingModule,
-    TranslateModule,
-    SharedModule,
-  ],
+  imports: [IonicModule, CommonModule, FormsModule, SearchPageRoutingModule, TranslateModule, SharedModule],
   declarations: [SearchPage, SearchResultComponent, NoResultsComponent, PlaceholderComponent, SearchDetailPage],
 })
 export class SearchPageModule {}
