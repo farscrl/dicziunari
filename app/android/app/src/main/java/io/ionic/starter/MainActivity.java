10d3d085d575198bf4edcbeff6d1fc4f04197891
Gion-Andri Cantieni <gion-andri@gmx.ch>
2021-12-08 15:38:59 +0100

    fix: crash on slpashscreen on some Android versions

--- app/android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ app/android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,6 +1,7 @@
 package io.ionic.starter;
 
 import android.content.pm.ActivityInfo;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.getcapacitor.BridgeActivity;
@@ -10,7 +11,10 @@ public class MainActivity extends BridgeActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    if(getResources().getBoolean(R.bool.portrait_only)){
+
+    // only setting for OS > than Android 8, as the splashscreen causes otherwise a crash:
+    // https://github.com/ionic-team/capacitor/issues/1627#issuecomment-652310622
+    if(getResources().getBoolean(R.bool.portrait_only) && android.os.Build.VERSION.SDK_INT > Build.VERSION_CODES.O){
       setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
   }
