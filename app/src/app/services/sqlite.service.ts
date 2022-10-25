16d9f0b8b97bf49b416171490dd418f78c7ab281
Gion-Andri Cantieni <gion-andri@gmx.ch>
2022-10-25 08:38:59 +0200

    feat: update sqlite web implementation

--- app/src/app/services/sqlite.service.ts
+++ app/src/app/services/sqlite.service.ts
@@ -112,18 +112,10 @@ export class SQLiteService {
   /**
    * addUpgradeStatement
    * @param database
-
    * @param toVersion
    * @param statements
-
    */
   async addUpgradeStatement(database: string, toVersion: number, statements: string[]): Promise<void> {
-
-
-
-
-
-
     if (this.sqlite != null) {
       try {
         const update: capSQLiteVersionUpgrade = { toVersion, statements};
@@ -322,7 +314,6 @@ export class SQLiteService {
                             console.log("Connection: " + value);
                         });
         */
-
         return Promise.resolve(myConns);
       } catch (err) {
         return Promise.reject(new Error(err));
