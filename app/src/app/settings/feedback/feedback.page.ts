6e8643c9aabbc2ad469a232f58f60c71896d98ac
Gion-Andri Cantieni <gion-andri@gmx.ch>
2022-10-25 10:39:17 +0200

    feat: add device info to feedback e-mail

--- app/src/app/settings/feedback/feedback.page.ts
+++ app/src/app/settings/feedback/feedback.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { ConfigService } from 'src/app/services/config.service';
 import { Locale } from 'src/data/search';
+import { Device } from '@capacitor/device';
 
 @Component({
   selector: 'app-feedback',
@@ -11,6 +12,8 @@ import { Locale } from 'src/data/search';
 export class FeedbackPage implements OnInit {
 
   public selectedLocale: Locale = Locale.rm;
+
+  public body: string = "";
 
   private localeSubscription: Subscription;
 
@@ -22,9 +25,22 @@ export class FeedbackPage implements OnInit {
     this.localeSubscription = this.configService.getLocaleObservable().subscribe((locale) => {
       this.selectedLocale = locale;
     });
+    this.loadDeviceInfo();
   }
 
   ngOnDestroy(): void {
     this.localeSubscription.unsubscribe();
+  }
+
+  private async loadDeviceInfo(): Promise<void> {
+    const info = await Device.getInfo();
+
+    this.body = "\n\n\n\n\n\n" +
+      "os: " + info.operatingSystem + "\n" +
+      "osVersion: " + info.osVersion + "\n" +
+      "model: " + info.model + "\n" +
+      "";
+
+    this.body = encodeURIComponent(this.body);
   }
 }
