674917663beaad85a5baf1b726f047d1527ac393
Gion-Andri Cantieni <gion-andri@gmx.ch>
2026-03-09 18:46:40 +0100

    chore: remove unused testing code

--- app/src/app/pipes/translate-cut.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TranslateCutPipe } from './translate-cut.pipe';
-
-describe('TranslateCutPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TranslateCutPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
